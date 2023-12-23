206c6fe08015cd0f533d166b6d67ca1d94ae3d0e
karelpaan <paan.karel@gmail.com>
2023-12-23 20:43:40 +0200

    function tests fixes

--- tests/FunctionTest.py
+++ tests/FunctionTest.py
@@ -4,7 +4,7 @@ from tests.TestDefinition import TestDefinition
 
 
 class FunctionTest(TestDefinition):
-    def __init__(self, name, arguments, description=None, expected_value=None, expected_count=None, points=0):
+    def __init__(self, name, arguments, where=None, description=None, expected_value=None, expected_count=None, points=0):
         super().__init__(
             name=name,
             points=points,
@@ -14,7 +14,13 @@ class FunctionTest(TestDefinition):
             expected_count=expected_count,
         )
 
-        self.query = f"SELECT * FROM {self.name}({self.arguments})"  # TODO implement parameters
+        query = f"SELECT * FROM {self.name}({self.arguments})"  # TODO implement parameters
+
+        # TODO right now a single where is possible (without a hack)
+        if where is not None:
+            query += f" WHERE ({where})"
+
+        self.query = query
 
     def execute(self, cursor):
         # TODO should the following be optional or added natively? ->
@@ -27,9 +33,17 @@ class FunctionTest(TestDefinition):
 
             if self.expected_value is None:
                 if self.expected_count is None:
-                    return super().response(len(result) > 0)
+                    return super().response(
+                        len(result) > 0,
+                        f"Correct count > 0 for function {self.name}({self.arguments})",
+                        f"Expected count > 0 for function {self.name}({self.arguments}) but none was found",
+                    )
                 else:
-                    return super().response(len(result) == self.expected_count)
+                    return super().response(
+                        len(result) == self.expected_count,
+                        f"The count is correct for function {self.name}({self.arguments}) -> {self.expected_count}",
+                        f"Expected count {self.expected_count} for function {self.name}({self.arguments}) but got {len(result)}",
+                    )
             else:
                 if type(result[0][0]) != type(self.expected_value):
                     return super().response(
