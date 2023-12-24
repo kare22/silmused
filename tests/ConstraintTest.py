b55919a63f0115bd2c6a47109b53289bf92e282c
karelpaan <paan.karel@gmail.com>
2023-12-24 20:28:53 +0200

    constraint test

---
+++ tests/ConstraintTest.py
@@ -0,0 +1,49 @@
+from tests.TestDefinition import TestDefinition
+
+
+class ConstraintTest(TestDefinition):
+    def __init__(self, name, constraint_name=None, constraint_type=None, description=None, should_exist=True, points=0):
+        query = f"SELECT * FROM information_schema.table_constraints WHERE table_name = '{self.name}'"
+
+        if constraint_name is not None:
+            query += f" AND constraint_name = '{constraint_name}'"
+        if constraint_type is not None:
+            query += f" AND constraint_type = '{constraint_type}'"
+
+        super().__init__(
+            name=name,
+            points=points,
+            description=description,
+            query=query,
+            should_exist=should_exist,
+        )
+
+        self.constraint_name = constraint_name
+        self.constraint_type = constraint_type
+
+        self.feedback = f"table {self.name}"
+
+        if self.constraint_name is not None:
+            self.feedback += f" with constraint name = '{self.constraint_name}'"
+        if self.constraint_type is not None:
+            if self.constraint_name is None:
+                self.feedback += f" with constraint type = '{self.constraint_type}'"
+            else:
+                self.feedback += f" and type = '{self.constraint_type}'"
+
+    def execute(self, cursor):
+        cursor.execute(self.query)
+        result = cursor.fetchall()
+
+        if self.should_exist:
+            return super().response(
+                len(cursor.fetchall()) == 0,
+                f"Expected to no find {self.feedback}",
+                f"Correct, {self.feedback} was not found",
+            )
+        else:
+            return super().response(
+                len(cursor.fetchall()) > 0,
+                f"Correct, {self.feedback} was found",
+                f"Expected to find {self.feedback} but none were found",
+            )
