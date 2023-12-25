7ff319b6541c3b55694cc3841081f1db00e07038
karelpaan <paan.karel@gmail.com>
2023-12-25 17:54:09 +0200

    runner

--- Runner.py
+++ Runner.py
@@ -1,6 +1,16 @@
+import os
 import subprocess
-
 import psycopg2
+from uuid import uuid4
+
+
+def _results_to_string(results):
+    string = ''
+    for result in results:
+        if result.get('type') is not 'execution' and result.get('message') is not None:
+            string += str(result.get('message')) + '\n'
+
+    print(string)
 
 
 class Runner:
@@ -14,32 +24,42 @@ class Runner:
         self.db_host = db_host
         self.db_port = db_port
 
-        self._create_db_from_backup()
-        self._run_tests()
+        self.db_name = f"db{'_' + self.test_name if self.test_name != '' else ''}_{self.file_path.split('/')[-1].split('.')[0]}_{str(uuid4()).replace('-', '_')}"
 
-    def _database_exists(self):
-        result = subprocess.run(
-            ["psql", "-U", self.db_user, "-tAc", f"SELECT 1 FROM pg_database WHERE datname='{self.db_name}'"], capture_output=True, text=True)
-        return result.stdout.strip() == '1'
+        if self._file_is_valid_pg_dump():
+            self._create_db_from_backup()
+            _results_to_string(self._run_tests())
+        else:
+            print('Error: File is not a valid PostgreSql dump file!')
 
-    def _drop_database(self):
-        subprocess.run(["dropdb", "-U", self.db_user, self.db_name])
+    def _file_is_valid_pg_dump(self):
+        if not os.path.isfile(self.file_path):
+            return False
+
+        try:
+            with open(self.file_path, 'rb') as file:
+                magic_number = file.read(5)
+                return magic_number == b'PGDMP'
+        except IOError:
+            return False
 
     def _create_db_from_backup(self):
-        self.db_name = f"db_{self.file_path.split('/')[-1].split('.')[0]}{'_' + self.test_name if self.test_name != '' else ''}"
-
-        if self._database_exists():
-            self._drop_database()
-
         subprocess.run(["createdb", "-U", self.db_user, self.db_name])
 
-        subprocess.run(["psql", "-U", self.db_user, "-d", self.db_name, "-c", "DROP SCHEMA public CASCADE;"])
+        subprocess.run(
+            ["psql", "-U", self.db_user, "-d", self.db_name, "-c", "DROP SCHEMA public CASCADE;"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
 
-        subprocess.run(["pg_restore", "-U", self.db_user, "-d", self.db_name, "--no-owner", "--no-acl", "-1", self.file_path])
+        subprocess.run(
+            ["pg_restore", "-U", self.db_user, "-d", self.db_name, "--no-owner", "--no-acl", "-1", self.file_path]
+        )
 
     def _run_tests(self):
         connection = self._connect()
         cursor = connection.cursor()
+
         results = []
 
         for test in self.tests:
@@ -60,3 +80,4 @@ class Runner:
         )
 
         return connection_layer
+
--- demo.py
+++ demo.py
@@ -4,5 +4,5 @@ from demo_test_cases import tests
 db_user = "karelpaan"
 file_path = "/Users/karelpaan/Projects/andmebaasid-auto-test/koik/Mihkel-Rump.backup"
 
-Runner(file_path, tests, test_name='uno', db_user=db_user)
+Runner(file_path, tests, test_name='unoduo', db_user=db_user)
 
