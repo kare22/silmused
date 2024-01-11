25aff43b0899271e35b9f7b0e76cad59566b52a2
karelpaan <paan.karel@gmail.com>
2024-01-11 11:45:47 +0200

    cli integration

--- silmused/cli-runner.py
+++ silmused/cli-runner.py
@@ -8,17 +8,19 @@ CLI to integrate the solution with e.g. a Docker sultion.
 NB! The tests file must include an array with the key "tests"!!
 '''
 def main():
-    sql_file_path = sys.argv[1]
-    tests_path = sys.argv[2]
-    lang = sys.argv[3] if len(sys.argv) > 3 else 'en'
-    db_user = sys.argv[4] if len(sys.argv) > 4 else None
-
-    with open(tests_path, 'r') as file:
-        tests_file_content = file.read()
-
-    exec(tests_file_content)
-
-    Runner(sql_file_path, tests, lang, db_user=db_user)
+    print("Hello, world!")
+# def main():
+#     sql_file_path = sys.argv[1]
+#     tests_path = sys.argv[2]
+#     lang = sys.argv[3] if len(sys.argv) > 3 else 'en'
+#     db_user = sys.argv[4] if len(sys.argv) > 4 else None
+#
+#     with open(tests_path, 'r') as file:
+#         tests_file_content = file.read()
+#
+#     exec(tests_file_content)
+#
+#     Runner(sql_file_path, tests, lang, db_user=db_user)
 
 if __name__ == "__main__":
     main()
