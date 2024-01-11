0bc0a47ebe4617f6b9c17b776da3111354d10b1a
karelpaan <paan.karel@gmail.com>
2024-01-11 10:43:49 +0200

    cli runner

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from demo_test_cases import tests
 from silmused.Runner import Runner
 
 db_user = "karelpaan"
-file_path = "/Users/karelpaan/Projects/andmebaasid-auto-test/koik/Mihkel-Rump.backup"
+file_path = "/Users/karelpaan/Projects/andmebaasid-auto-test/koik/test.backup"
 
 Runner(file_path, tests, test_name='unoduo', db_user=db_user)
 
