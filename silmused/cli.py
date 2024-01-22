88a9c9f4759c4095db4b1f4696a6215868643549
Martti <marttikakk@hotmail.com>
2024-01-22 15:08:28 +0200

    cli changes

--- silmused/cli.py
+++ silmused/cli.py
@@ -13,7 +13,9 @@ def main():
     sql_file_path = sys.argv[1]
     tests_path = sys.argv[2]
     db_user = sys.argv[3] if len(sys.argv) > 3 else 'postgres'
-    lang = sys.argv[4] if len(sys.argv) > 4 else 'en'
+    db_host = sys.argv[4] if len(sys.argv) > 4 else 'localhost'
+    db_port = sys.argv[5] if len(sys.argv) > 5 else '5432'
+    db_password = sys.argv[6] if len(sys.argv) > 6 else 'postgresql'
     
     with open(tests_path, 'r') as file:
         tests_file_content = file.read()
@@ -22,5 +24,5 @@ def main():
     exec(tests_file_content, globals(), local_vars)
     tests = local_vars.get('tests')
 
-    r=Runner(sql_file_path, tests, lang, db_user=db_user)
+    r=Runner(sql_file_path, tests, db_user=db_user, db_host=host, db_port=db_port, db_password=db_password)
     print(r.get_results())
