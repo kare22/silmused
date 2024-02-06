b5aa8c65817c005ca22414f5703756496f4b4329
Martti <marttikakk@hotmail.com>
2024-02-06 12:54:10 +0200

    data in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     license='MIT License',
     author='Karel Paan, Martti Kakk',
     author_email='paan.karel@gmail.com',
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(where='silmused'),
+    package_dir={"": "silmused"},
+    package_data={"locale": ["*.json"]},
     url='https://github.com/kare22/silmused',
     install_requires=[
         'psycopg2'
@@ -17,7 +19,5 @@ setup(
             'silmused=silmused.cli:main',
         ],
     },
-    data_files=[
-        ('locale', ['locale/*.json']),
-    ]
+
 )
