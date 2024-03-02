0b038ada69d0437231a1846be877dab33c0485c4
Martti <marttikakk@hotmail.com>
2024-03-02 13:33:52 +0200

    current tests

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
 # from silmused.test_cases.feedback_tests import tests
-from silmused.test_cases.kodutood_koik import tests
+#from silmused.test_cases.kodutood_koik import tests
+from silmused.test_cases.praksid_koik import tests
 # from silmused.test_cases.demo_test_cases import tests
 from silmused.Runner import Runner
 
 db_user = "postgres"
 # db_user = "karelpaan"
-file_path = "silmused/test_cases/kodutoo4.sql"
+file_path = "silmused/test_cases/praktikum8.sql"
 #file_path = "silmused/test_cases/feedback_tests.sql"
 
 r = Runner(file_path, tests, db_user=db_user, lang='et')
