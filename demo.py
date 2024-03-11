91272591d886b518d88aa7caa3c5b3ee1f632394
Martti <marttikakk@hotmail.com>
2024-03-11 21:05:54 +0200

    bug fixes
    StructureTest multi column checks gives wrong result.
    DataTest NULL values are taken as a success, but shouldn't be

--- demo.py
+++ demo.py
@@ -1,12 +1,13 @@
 # from silmused.test_cases.feedback_tests import tests
-#from silmused.test_cases.kodutood_koik import tests
-from silmused.test_cases.praksid_koik import tests
+from silmused.test_cases.kodutood_koik import tests
+#from silmused.test_cases.praksid_koik import tests
 # from silmused.test_cases.demo_test_cases import tests
 from silmused.Runner import Runner
 
 db_user = "postgres"
 # db_user = "karelpaan"
-file_path = "silmused/test_cases/praktikum8.sql"
+#file_path = "silmused/test_cases/praktikum8.sql"
+file_path= "silmused/test_cases/tudeng.sql"
 #file_path = "silmused/test_cases/feedback_tests.sql"
 
 r = Runner(file_path, tests, db_user=db_user, lang='et')
