a5e8ed8e2353f1474bb317e79a1b9316cd487c67
Martti <marttikakk@hotmail.com>
2024-03-30 14:31:34 +0200

    added est feedback

--- demo.py
+++ demo.py
@@ -11,6 +11,6 @@ file_path = "silmused/test_cases/kodutoo5.sql"
 #file_path= "silmused/test_cases/tudeng.sql"
 #file_path = "silmused/test_cases/feedback_tests.sql"
 
-r = Runner(file_path, tests, db_user=db_user, lang='en')
+r = Runner(file_path, tests, db_user=db_user, lang='et')
 
 print(r.get_results())
