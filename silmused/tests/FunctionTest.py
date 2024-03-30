04413e61f6b09ec3608ae2254d2bb61157b38498
Martti <marttikakk@hotmail.com>
2024-03-30 11:09:04 +0200

    func and proc test feedbacks
    smaller tests were added as def functions

--- silmused/tests/FunctionTest.py
+++ silmused/tests/FunctionTest.py
@@ -3,7 +3,8 @@ from silmused.utils import list_to_string
 
 
 class FunctionTest(TestDefinition):
-    def __init__(self, name, title=None, arguments=None, column_name=None, where=None, description=None, expected_value=None, expected_count=None, number_of_parameters=None, points=0):
+    def __init__(self, name, title=None, arguments=None, column_name=None, where=None, description=None, expected_value=None,
+                 expected_count=None, number_of_parameters=None, points=0):
         super().__init__(
             name=name,
             title=title,
@@ -19,36 +20,23 @@ class FunctionTest(TestDefinition):
         self.number_of_parameters = number_of_parameters
 
     def execute(self, cursor):
-        # TODO small tests in separate functions
 
         # Check that function name exists
-        cursor.execute(f"SELECT * FROM pg_catalog.pg_proc WHERE proname='{self.name}'")
-        if len(cursor.fetchall()) <= 0:
-            return super().response(
-                False,
-                ''
-                f"Function {self.name} was not found"
-            )
+        test_function_exists_result = self.test_function_exists(cursor)
+        if test_function_exists_result is not None:
+            return test_function_exists_result
+
+        # Check that type is correct
+        test_function_type_result = self.test_function_type(cursor)
+        if test_function_type_result is not None:
+            return test_function_type_result
 
         # Check that function number of arguments is correct
         if self.number_of_parameters is not None:
-            cursor.execute(f"SELECT pronargs FROM pg_catalog.pg_proc WHERE proname='{self.number_of_parameters}'")
-            number_of_parameters_result = cursor.fetchall()[0][0]
+            test_function_args_result = self.test_function_args(cursor)
+            if test_function_args_result is not None:
+                return test_function_args_result
 
-            if not number_of_parameters_result == self.number_of_parameters:
-                return super().response(
-                    False,
-                    ''
-                    f"Expected {self.number_of_parameters} for function {self.name} but found {number_of_parameters_result}"
-                )
-        # Check that type is correct
-        cursor.execute(f"SELECT routine_name FROM information_schema.routines WHERE routine_type = 'FUNCTION' AND routine_name='{self.name}'")
-        if not len(cursor.fetchall()) > 0:
-            return super().response(
-                False,
-                ''
-                f"Expected function {self.name} to be of type FUNCTION"
-            )
 
         cursor.execute(self.query)
         result = cursor.fetchall()
@@ -57,14 +45,22 @@ class FunctionTest(TestDefinition):
             if self.expected_count is None:
                 return super().response(
                     len(result) > 0,
-                    f"Correct count > 0 for function {self.name}({list_to_string(self.arguments)})",
-                    f"Expected count > 0 for function {self.name}({list_to_string(self.arguments)}) but none was found",
+                    {"test_type": "function_test",
+                     "test_key": "function_not_expected_value_not_expected_result_count_positive_feedback",
+                     "params": [self.name, list_to_string(self.arguments)]},
+                    {"test_type": "function_test",
+                     "test_key": "function_not_expected_value_not_expected_result_count_negative_feedback",
+                     "params": [self.name, list_to_string(self.arguments)]}
                 )
             else:
                 return super().response(
                     len(result) == self.expected_count,
-                    f"The count is correct for function {self.name}({list_to_string(self.arguments)}) -> {self.expected_count}",
-                    f"Expected count {self.expected_count} for function {self.name}({list_to_string(self.arguments)}) but got {len(result)}",
+                    {"test_type": "function_test",
+                     "test_key": "function_not_expected_value_expected_result_count_positive_feedback",
+                     "params": [self.expected_count, self.name, list_to_string(self.arguments)]},
+                    {"test_type": "function_test",
+                     "test_key": "function_not_expected_value_expected_result_count_negative_feedback",
+                     "params": [self.expected_count, self.name, list_to_string(self.arguments), len(result)]}
                 )
         else:
             # TODO when type is decimal but we give float, we get an error
@@ -76,6 +72,55 @@ class FunctionTest(TestDefinition):
             #     )
             return super().response(
                 str(result[0][0]) == str(self.expected_value),
-                'Correct',
-                f"Expected {self.expected_value} but got {result[0][0]}",
+                {"test_type": "function_test",
+                 "test_key": "function_expected_value_positive_feedback",
+                 "params": [self.expected_value]},
+                {"test_type": "function_test",
+                 "test_key": "function_expected_value_negative_feedback",
+                 "params": [self.expected_value, result[0][0]]}
             )
+
+    def test_function_exists(self, cursor):
+        cursor.execute(f"SELECT * FROM pg_catalog.pg_proc WHERE proname='{self.name}'")
+        if len(cursor.fetchall()) <= 0:
+            return super().response(
+                False,
+                {"test_type": "function_test",
+                 "test_key": "function_exists_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "function_test",
+                 "test_key": "function_exists_negative_feedback",
+                 "params": [self.name]}
+            )
+        return None
+
+    def test_function_type(self, cursor):
+        cursor.execute(
+            f"SELECT routine_name FROM information_schema.routines WHERE routine_type = 'FUNCTION' AND routine_name='{self.name}'")
+        if not len(cursor.fetchall()) > 0:
+            return super().response(
+                False,
+                {"test_type": "function_test",
+                 "test_key": "function_type_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "function_test",
+                 "test_key": "function_type_negative_feedback",
+                 "params": [self.name]}
+            )
+        return None
+
+    def test_function_args(self, cursor):
+        cursor.execute(f"SELECT pronargs FROM pg_catalog.pg_proc WHERE proname='{self.name}'")
+        number_of_parameters_result = cursor.fetchall()[0][0]
+        if not number_of_parameters_result == self.number_of_parameters:
+            return super().response(
+                False,
+                {"test_type": "function_test",
+                 "test_key": "function_parameters_amount_positive_feedback",
+                 "params": [self.name]}
+                ,
+                {"test_type": "function_test",
+                 "test_key": "function_parameters_amount_negative_feedback",
+                 "params": [self.number_of_parameters, self.name, number_of_parameters_result]}
+            )
+        return None
--- silmused/tests/ProcedureTest.py
+++ silmused/tests/ProcedureTest.py
@@ -3,7 +3,8 @@ from silmused.utils import list_to_string
 
 
 class ProcedureTest(TestDefinition):
-    def __init__(self, name, arguments, title=None, number_of_columns=None, description=None, expected_value=None, expected_count=None, pre_query=None, after_query=None, points=0):
+    def __init__(self, name, arguments, title=None, description=None, expected_value=None,
+                 expected_count=None, number_of_parameters=None, pre_query=None, after_query=None, points=0):
         if after_query is None:
             raise Exception('Parameter "after_query" is required')
 
@@ -20,13 +21,25 @@ class ProcedureTest(TestDefinition):
             after_query=after_query,
         )
 
-        self.number_of_columns = number_of_columns
+        self.number_of_parameters = number_of_parameters
 
     def execute(self, cursor):
-        if self.number_of_columns is not None:
-            cursor.execute(f"SELECT pronargs FROM pg_catalog.pg_proc WHERE proname = '{self.name}'")
-            if not cursor.fetchall()[0][0] == self.number_of_columns:
-                return False, f"Procedure's \"{self.name}\" number of parameters is wrong",
+
+        # Check that procedure name exists
+        test_procedure_exists_result = self.test_procedure_exists(cursor)
+        if test_procedure_exists_result is not None:
+            return test_procedure_exists_result
+
+        # Check that type is correct
+        test_procedure_type_result = self.test_procedure_type(cursor)
+        if test_procedure_type_result is not None:
+            return test_procedure_type_result
+
+        # Check that procedure number of arguments is correct
+        if self.number_of_parameters is not None:
+            test_procedure_args_result = self.test_procedure_args(cursor)
+            if test_procedure_args_result is not None:
+                return test_procedure_args_result
 
         if self.pre_query is not None:
             cursor.execute(self.pre_query)
@@ -37,8 +50,72 @@ class ProcedureTest(TestDefinition):
 
         result = cursor.fetchall()
 
-        return super().response(
-            len(result) > 0,
-            f"Correct count > 0 for procedure \"{self.name}({list_to_string(self.arguments)})\"",
-            f"Expected count > 0 for procedure \"{self.name}({list_to_string(self.arguments)})\" but none was found",
-        )
+        if self.expected_count is None:
+            return super().response(
+                len(result) > 0,
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_not_expected_result_count_negative_feedback",
+                 "params": [self.name, list_to_string(self.arguments)]},
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_not_expected_result_count_negative_feedback",
+                 "params": [self.name, list_to_string(self.arguments)]}
+            )
+        else:
+            return super().response(
+                len(result) == self.expected_count,
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_expected_result_count_negative_feedback",
+                 "params": [self.expected_count, self.name, list_to_string(self.arguments), len(result)]},
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_expected_result_count_negative_feedback",
+                 "params": [self.expected_count, self.name, list_to_string(self.arguments), len(result)]}
+            )
+        # return super().response(
+        #    len(result) > 0,
+        #    f"Correct count > 0 for procedure \"{self.name}({list_to_string(self.arguments)})\"",
+        #    f"Expected count > 0 for procedure \"{self.name}({list_to_string(self.arguments)})\" but none was found",
+        # )
+
+    def test_procedure_exists(self, cursor):
+        cursor.execute(f"SELECT * FROM pg_catalog.pg_proc WHERE proname='{self.name}'")
+        if len(cursor.fetchall()) <= 0:
+            return super().response(
+                False,
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_exists_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_exists_negative_feedback",
+                 "params": [self.name]}
+            )
+        return None
+
+    def test_procedure_type(self, cursor):
+        cursor.execute(
+            f"SELECT routine_name FROM information_schema.routines WHERE routine_type = 'PROCEDURE' AND routine_name='{self.name}'")
+        if not len(cursor.fetchall()) > 0:
+            return super().response(
+                False,
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_type_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_type_negative_feedback",
+                 "params": [self.name]}
+            )
+        return None
+
+    def test_procedure_args(self, cursor):
+        cursor.execute(f"SELECT pronargs FROM pg_catalog.pg_proc WHERE proname='{self.name}'")
+        number_of_parameters_result = cursor.fetchall()[0][0]
+        if not number_of_parameters_result == self.number_of_parameters:
+            return super().response(
+                False,
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_parameters_exists_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "procedure_test",
+                 "test_key": "procedure_parameters_exists_negative_feedback",
+                 "params": [self.number_of_parameters, self.name, number_of_parameters_result]}
+            )
+        return None
