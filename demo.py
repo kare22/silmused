b935904a52777c45cdb8963606a8640e7d82842c
Martti <marttikakk@hotmail.com>
2024-08-21 20:05:26 +0300

    small updates

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@
 #from silmused.test_cases.kodutood_koik import tests
 from silmused.test_cases.yldkontroll import tests
 #from silmused.test_cases.single_test import tests
+#from silmused.test_cases.eurovisioon import tests
 # from silmused.test_cases.demo_test_cases import tests
 from silmused.Runner import Runner
 import sys
@@ -10,12 +11,13 @@ import sys
 db_user = "postgres"
 lang = 'et'
 # db_user = "karelpaan"
-#file_path = "silmused/test_cases/praktikum12.sql"
-file_path = "silmused/test_cases/kodutoo6.sql"
+file_path = "silmused/test_cases/praktikum12.sql"
+#file_path = "silmused/test_cases/kodutoo6.sql"
+#file_path = "silmused/test_cases/eurovisioon.sql"
 #file_path= "silmused/test_cases/tudeng.sql"
 #file_path = "silmused/test_cases/feedback_tests.sql"
 
 
-r = Runner(file_path, tests, db_user=db_user, lang=lang, encoding='')
+r = Runner(file_path, tests, db_user=db_user, lang=lang, encoding='UTF-8')
 
 print(r.get_results())
