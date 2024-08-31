f51e4d7a11fc09777e37047fd560e7a0fa99874b
Martti <marttikakk@hotmail.com>
2024-08-31 14:41:06 +0300

    Fixed errors and feedbacks
    Fixed bad feedback in case of indexError
    Added feedback when query result amount = 0 - caused index errors also

--- query_demo.py
+++ query_demo.py
@@ -1,14 +1,14 @@
 # from silmused.test_cases.query_tests.paring_koik import tests
-#from silmused.test_cases.query_tests.euro_praks_6_2 import tests
-from silmused.test_cases.query_tests.euro_kodu_6_4 import tests
-from silmused.test_cases.query_tests.euro_querys import querys
+#from silmused.test_cases.query_tests.eurovisioon_db.euro_praks_3_1 import tests
+from silmused.test_cases.query_tests.eurovisioon.euro_kodu_2_1 import tests
+from silmused.test_cases.query_tests.eurovisioon.euro_querys import querys
 from silmused.Runner import Runner
 
 db_user = "postgres"
 test_query = 'query'
 lang = 'et'
-#base_database_path = "silmused/test_cases/createdb.sql"
-base_database_path = "silmused/test_cases/eurovisioon.sql"
+base_database_path = "silmused/test_cases/ope_createdb.sql"
+base_database_path = "silmused/test_cases/query_tests/eurovisioon/eurovisioon.sql"
 query_sql_path = "silmused/test_cases/query.sql"
 query_sql = ''
 if test_query == 'query':
