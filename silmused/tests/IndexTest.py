b2bde9c99fb260fb77ab27879b12eb0e96d8b0fb
Martti <marttikakk@hotmail.com>
2024-12-21 15:26:10 +0200

    custom feedback for index and procedure tests

--- silmused/tests/IndexTest.py
+++ silmused/tests/IndexTest.py
@@ -2,25 +2,36 @@ from silmused.tests.TestDefinition import TestDefinition
 
 
 class IndexTest(TestDefinition):
-    def __init__(self, name, title=None, description=None, points=0):
+    def __init__(self, name, title=None, description=None, custom_feedback=None, points=0):
         super().__init__(
             name=name,
             title=title,
             points=points,
             description=description,
+            custom_feedback=custom_feedback,
             query=f"SELECT * FROM pg_indexes WHERE indexname = '{name}'",
         )
 
     def execute(self, cursor):
         cursor.execute(self.query)
         result = cursor.fetchall()
-
-        return super().response(
-            len(result) > 0,
-            {"test_type": "index_test",
-             "test_key": "index_positive_feedback",
-             "params": [self.name]},
-            {"test_type": "index_test",
-             "test_key": "index_negative_feedback",
-             "params": [self.name]}
-        )
+        if self.custom_feedback is None:
+            return super().response(
+                len(result) > 0,
+                {"test_type": "index_test",
+                 "test_key": "index_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "index_test",
+                 "test_key": "index_negative_feedback",
+                 "params": [self.name]}
+            )
+        else:
+            return super().response(
+                len(result) > 0,
+                {"test_type": "index_test",
+                 "test_key": "custom_feedback",
+                 "params": [self.custom_feedback]},
+                {"test_type": "index_test",
+                 "test_key": "custom_feedback",
+                 "params": [self.custom_feedback]},
+            )
