2e3e681f2686f467bd29e26bc3992b2ab228d68f
Martti <marttikakk@hotmail.com>
2024-12-21 16:22:06 +0200

    structure, trigger adn view tests support custom feedback

--- silmused/tests/TriggerTest.py
+++ silmused/tests/TriggerTest.py
@@ -2,13 +2,15 @@ from silmused.tests.TestDefinition import TestDefinition
 
 
 class TriggerTest(TestDefinition):
-    def __init__(self, name, title=None, description=None, points=0, arguments=None, action_timing=None):
+    def __init__(self, name, title=None, description=None, points=0, arguments=None,
+                 custom_feedback=None, action_timing=None):
         super().__init__(
             name=name,
             title=title,
             points=points,
             description=description,
             arguments=arguments,
+            custom_feedback=custom_feedback,
             query=f"SELECT * FROM information_schema.views WHERE table_name = '{name}'",
         )
 
@@ -23,30 +25,50 @@ class TriggerTest(TestDefinition):
 
         # Check trigger event manipulation on arguments and trigger action timing
         errors = self.test_trigger_manipulation(cursor) if len(self.arguments) > 0 else []
-
-        return super().response(
-            len(errors) == 0,
-            {"test_type": "trigger_test",
-             "test_key": "trigger_definition_positive_feedback",
-             "params": [self.name]},
-            {"test_type": "trigger_test",
-             "test_key": "trigger_definition_negative_feedback",
-             "params": [self.name, {', '.join(errors)}]},  # TODO errorid on hetkel inglise keeles
-        )
-
+        if self.custom_feedback is None:
+            return super().response(
+                len(errors) == 0,
+                {"test_type": "trigger_test",
+                 "test_key": "trigger_definition_positive_feedback",
+                 "params": [self.name]},
+                {"test_type": "trigger_test",
+                 "test_key": "trigger_definition_negative_feedback",
+                 "params": [self.name, {', '.join(errors)}]},  # TODO errorid on hetkel inglise keeles
+            )
+        else:
+            return super().response(
+                False,
+                {"test_type": "trigger_test",
+                 "test_key": "custom_feedback",
+                 "params": [self.custom_feedback]},
+                {"test_type": "trigger_test",
+                 "test_key": "custom_feedback",
+                 "params": [self.custom_feedback]},
+            )
     def test_trigger_exists(self, cursor):
         cursor.execute(f"SELECT trigger_name FROM information_schema.triggers WHERE trigger_name = '{self.name}'")
 
         if len(cursor.fetchall()) <= 0:
-            return super().response(
-                False,
-                {"test_type": "trigger_test",
-                 "test_key": "trigger_exists_positive_feedback",
-                 "params": [self.name]},
-                {"test_type": "trigger_test",
-                 "test_key": "trigger_exists_negative_feedback",
-                 "params": [self.name]},
-            )
+            if self.custom_feedback is None:
+                return super().response(
+                    False,
+                    {"test_type": "trigger_test",
+                     "test_key": "trigger_exists_positive_feedback",
+                     "params": [self.name]},
+                    {"test_type": "trigger_test",
+                     "test_key": "trigger_exists_negative_feedback",
+                     "params": [self.name]},
+                )
+            else:
+                return super().response(
+                    False,
+                    {"test_type": "trigger_test",
+                     "test_key": "custom_feedback",
+                     "params": [self.custom_feedback]},
+                    {"test_type": "trigger_test",
+                     "test_key": "custom_feedback",
+                     "params": [self.custom_feedback]},
+                )
         return None
 
     def test_trigger_manipulation(self, cursor):
