440e15748a8f04845211160eba52410d9ae676df
Martti <marttikakk@hotmail.com>
2025-10-26 08:57:47 +0200

    encoding can be None now

--- silmused/Runner.py
+++ silmused/Runner.py
@@ -23,7 +23,7 @@ def _results_to_string(results):
 
 class Runner:
     def __init__(self, backup_file_path, tests, lang='en', test_name='', db_user='postgres', db_host='localhost',
-                 db_password='postgres', db_port='5432', test_query='test', query_sql=None, encoding=None):
+                 db_password='postgres', db_port='5432', test_query='test', query_sql='', encoding=None):
         self.file_path = backup_file_path
         self.tests = tests
         self.test_name = test_name
@@ -97,20 +97,14 @@ class Runner:
         if not self.file_path.lower().endswith('.sql'):
             return False
 
-        if len(self.encoding) > 0:
-            try:
-                with open(self.file_path, 'r', encoding=self.encoding) as file:
-                    lines = file.readlines()
-                    return any(line.strip().startswith("INSERT") for line in lines)
-            except IOError:
-                return False
-        else:
-            try:
-                with open(self.file_path, 'r') as file:
-                    lines = file.readlines()
-                    return any(line.strip().startswith("INSERT") for line in lines)
-            except IOError:
-                return False
+        #if len(self.encoding) > 0:
+        try:
+            with open(self.file_path, 'r', encoding=self.encoding) as file:
+                lines = file.readlines()
+                return any(line.strip().startswith("INSERT") for line in lines)
+        except IOError:
+            return False
+
 
     def _create_db_from_psql_insert(self):
         connection = self._connect(db_name='postgres')
@@ -129,13 +123,9 @@ class Runner:
         cursor = connection.cursor()
 
         try:
-            if len(self.encoding) > 0:
-                with open(self.file_path, 'r', encoding=self.encoding) as file:
-                    sql_script = file.read()
-
-            else:
-                with open(self.file_path, 'r') as file:
-                    sql_script = file.read()
+            #if len(self.encoding) > 0:
+            with open(self.file_path, 'r', encoding=self.encoding) as file:
+                sql_script = file.read()
 
             # Postgre 17.6+ added rstrict/unrestrict to dump files and psycopg2 script execution fails, if these are not removed
             if re.findall(r"\\restrict", sql_script):
