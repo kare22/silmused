f0c6998298f3f592f666e3368709ceb8283bcdde
Martti <marttikakk@hotmail.com>
2026-01-31 13:39:54 +0200

    Added column_name_fallback if some columns can be slightly different from required
    Updated bunch of tests and column_name_fallback argument to DataTests

--- silmused/test_cases/query_tests/male/male_kodu_1_1.py
+++ silmused/test_cases/query_tests/male/male_kodu_1_1.py
@@ -9,12 +9,25 @@ tests = [
                 title='Kas on olemas veerg eesnimi?',
                 name='query_test',
                 column_name='eesnimi',
-                points=20,
+                points=15,
             ),
             QueryStructureTest(
                 title='Kas on olemas veerg perenimi?',
                 name='query_test',
                 column_name='perenimi',
+                points=15,
+            ),
+            QueryStructureTest(
+                title='Kas lahenduses on ainult 2 veergu?',
+                name='query_test',
+                column_name=['id','isikukood','klubi','synniaeg','sugu','ranking'],
+                should_exist=False,
+                points=10,
+            ),
+            QueryStructureTest(
+                title='Kas lahenduses on kasutatud LIMITit?',
+                name='query_test',
+                elements='LIMIT',
                 points=20,
             ),
             QueryDataTest(
@@ -30,7 +43,7 @@ tests = [
                 column_name='eesnimi',
                 where="test_id=1",
                 expected_value="Sander",
-                points=30,
+                points=20,
             ),
             QueryDataTest(
                 title='Kas viies noorim on´õige isik?',
@@ -38,7 +51,7 @@ tests = [
                 column_name='eesnimi',
                 where="test_id=5",
                 expected_value="Keiu",
-                points=30,
+                points=20,
             ),
         ]
     ),
