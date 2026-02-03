a12bdfad1e4b7c1e742aa4c9c4c135d0d530919d
Martti <marttikakk@hotmail.com>
2026-02-03 21:30:15 +0200

    Added more specific feedback when column_name or elements are in list
    For example if required columns are id, name, age and for example only age is missing, then feedback would only be that column age is missing. Previously it would return the full list.

--- silmused/tests/ViewTest.py
+++ silmused/tests/ViewTest.py
@@ -52,6 +52,10 @@ class ViewTest(TestDefinition):
         self.isMaterialized = isMaterialized
 
     def execute(self, cursor):
+        if isinstance(self.elements, list):
+            self.query = self._check_separately_for_all_elements(cursor)
+        if isinstance(self.column_name, list):
+            self.query = self._check_separately_for_all_columns(cursor)
         cursor.execute(self.query)
         result = cursor.fetchall()
 
@@ -318,3 +322,44 @@ class ViewTest(TestDefinition):
                              "test_key": "custom_feedback",
                              "params": [self.custom_feedback]},
                         )
+
+    def _check_separately_for_all_elements(self, cursor):
+        found = []
+        for element in self.elements:
+            query = f"SELECT * FROM information_schema.views WHERE table_name = 'query_view' AND view_definition ILIKE '%{element}%'"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            if self.should_exist and len(result) == 0:
+                found.append(element)
+            elif not self.should_exist and len(result) > 0:
+                found.append(element)
+        if len(found) == 0: return self.query
+        if self.isMaterialized:
+            query = f"SELECT * FROM pg_matviews WHERE matviewname = '{self.name}'"
+        else:
+            query = f"SELECT * FROM pg_views WHERE viewname = '{self.name}'"
+        self.elements = found
+        for (index, arg) in enumerate(self.elements):
+            operator = 'AND (' if index == 0 else 'OR'
+            query += f" {operator} view_definition ILIKE '%{arg}%'"
+        query += ")"
+        return query
+
+    def _check_separately_for_all_columns(self, cursor):
+        found = []
+        for column in self.column_name:
+            query = f"SELECT * FROM information_schema.columns WHERE table_name = '{self.name}' AND column_name = '{column}'"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            if self.should_exist and len(result) == 0:
+                found.append(column)
+            elif not self.should_exist and len(result) > 0:
+                found.append(column)
+        if len(found) == 0: return self.query
+        query = f"SELECT * FROM information_schema.columns WHERE table_name = '{self.name}'"
+        self.column_name = found
+        for (index, c_name) in enumerate(self.column_name):
+            operator = 'AND (' if index == 0 else 'OR'
+            query += f" {operator} column_name = '{c_name}'"
+        query += ")"
+        return query
